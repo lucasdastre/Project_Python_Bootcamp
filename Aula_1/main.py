ff3d57f8748833ea85141433375e8a3889dcf5fb
Lucas <lucasdastre10@gmail.com>
2025-09-17 20:01:45 -0300

    Input de dois valores

--- Aula_1/main.py
+++ Aula_1/main.py
@@ -8,3 +8,11 @@ x = input("Digite seu nome: \n")
 #Digite o seu nome e retorn o numeoro de carcaterers
 print(f"Seu nome é {x}")
 print(len(x))
+
+
+#Crie um programa onde o usuario digita dois valores
+
+y = int(input("Digite Primeiro Valor: \n"))
+z = int(input("Digite Segundo Valor: \n"))
+
+print(f'A soma dos valores é {y+z}')
