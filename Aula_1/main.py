96e03727d814b5880f8596dc6a59d95bc3bbc3d3
Lucas <lucasdastre10@gmail.com>
2025-09-17 20:07:57 -0300

    primeiro aula terminada

--- Aula_1/main.py
+++ Aula_1/main.py
@@ -16,3 +16,19 @@ y = int(input("Digite Primeiro Valor: \n"))
 z = int(input("Digite Segundo Valor: \n"))
 
 print(f'A soma dos valores é {y+z}')
+
+#temos 4 tipos primitivos
+
+numero = 3 #int
+numero_decimal = 4.9 #float
+verdadeiro = True #bool
+falso = False #bool
+texto = "Lucas" #string
+#em python tudo é objeto
+print(type(numero))
+#python é case sensitive
+
+_variavel_secreta = 10
+#pra trazer mais seriedade a essa variavel
+
+#o python tem uma tipagem dinamica , e isso é importante levar em consideração
