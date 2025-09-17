cf35fafa1eecdfc85554fa811b9bf5f68277cd66
Lucas <lucasdastre10@gmail.com>
2025-09-17 19:58:22 -0300

    Primeiros testes

---
+++ Aula_1/main.py
@@ -0,0 +1,10 @@
+#Aula 1 - Bootcamp Python
+
+print("Primeiro código do bootcamp")
+
+x = input("Digite seu nome: \n")
+
+
+#Digite o seu nome e retorn o numeoro de carcaterers
+print(f"Seu nome é {x}")
+print(len(x))
