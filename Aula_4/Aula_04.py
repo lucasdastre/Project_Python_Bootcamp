ea2445665a1e07559d0b0c243d1564b32a725d36
Lucas <lucasdastre10@gmail.com>
2025-09-22 22:49:04 -0300

    teste

--- Aula_4/Aula_04.py
+++ Aula_4/Aula_04.py
@@ -1,6 +1,10 @@
 #O python tem inferencia automatica de variaveis
 #tipagem dinamica
 
+from typing import Dict
+import json
+
+
 idade : int = 30
 nome : str= "Lucas"
 dinheiro : float = 15.4
@@ -25,3 +29,58 @@ print(produtos)
 
 produtos.pop(-2)
 print(produtos)
+
+
+#Exemplo Dicionario
+# Chave : Valor
+
+produto_1 = {
+                "nome" : "Sapato",
+                "Tamanho" : 39,
+                "Cor": "Marrom",
+                "Disponibilidade" : True
+}
+
+
+produto_2= {
+                "nome" : "tenis",
+                "Tamanho" : 42,
+                "Cor": "azul",
+                "Disponibilidade" : False
+}
+
+#Lista de items de dicionario
+
+wish_list = []
+wish_list.append(produto_1)
+wish_list.append(produto_2)
+print(wish_list)
+
+#Transformando em json , um dicionario co particularidade da linguagem java script
+wish_list_json = json.dumps(wish_list)
+print(wish_list_json)
+
+#Crie um dicionario que colete informações de um livro
+#Titulo , autor , ano de publicação
+
+Livros : list = []
+
+ficar = True
+
+while ficar:
+    info = {
+    "Livro" : str(input("Digite o nome do Livro :")),
+    "Autor" : str(input("Digite o nome do Autor :")) , 
+    "Ano" : str(input("Digite o nome do Ano :"))
+    }
+
+    resp = input("Voce deseja continuar no programa? : ")
+    Livros.append(info)
+
+    if resp == "Sim":
+        ficar = False
+        break
+    else:
+        continue
+        
+print(Livros)
