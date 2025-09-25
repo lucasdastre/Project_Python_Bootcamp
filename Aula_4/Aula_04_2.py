bc5669195b339c86c5a1a7c6d8b0c463b831089d
Lucas <lucasdastre10@gmail.com>
2025-09-25 20:47:33 -0300

    mostrando funcoes

--- Aula_4/Aula_04_2.py
+++ Aula_4/Aula_04_2.py
@@ -11,13 +11,32 @@ A primeira linha do CSV deve ser o cabeçalho com os nomes das colunas.
 No final, arquivo_csv vai ser uma lista de dicionários, ex.:'''
 
 #Gerenciador de contextos
-with open(caminho_arquivo , mode = 
-          'r' , encoding = 'utf-8') as arquivo:
-    leitor_csv =csv.DictReader(arquivo)
+# with open(caminho_arquivo , mode = 
+#           'r' , encoding = 'utf-8') as arquivo:
+#     leitor_csv =csv.DictReader(arquivo)
 
-    for linha in leitor_csv:
-        arquivo_csv.append(linha)
+#     for linha in leitor_csv:
+#         arquivo_csv.append(linha)
 
-    print(arquivo_csv)
+#     print(arquivo_csv)
 
+
+
+    #funcoes
+
+    #Empacotar uma funcao
+
+lista_de_num = [1,2,3,4,1000,155, -500] 
+
+def ordenar_lista(numero : list) -> list:
+    nova_lista_numero = numero.copy()
+
+    for i in range(len(nova_lista_numero)): 
+        for j in range(i+1 , len(nova_lista_numero)):
+            if nova_lista_numero[i] > nova_lista_numero[j]:
+                nova_lista_numero[i] , nova_lista_numero[j] = nova_lista_numero[j] , nova_lista_numero[i]
     
+    return nova_lista_numero
+
+nova_lista = ordenar_lista(lista_de_num)
+print(nova_lista)
