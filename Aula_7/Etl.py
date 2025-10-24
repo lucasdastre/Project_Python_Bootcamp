29fc3196589a40d263a5d9de39db854162f49a39
Lucas <lucasdastre10@gmail.com>
2025-10-23 22:09:42 -0300

    feat/ adicao de funcoes de dataframe da aula 7

--- Aula_7/Etl.py
+++ Aula_7/Etl.py
@@ -1,12 +1,13 @@
 import csv
+import pandas as pd
 
-
-path = "vendas.csv"
+path = "Aula_7/vendas.csv"
 
 def ler_Csv(path) -> list:
     with open(path , mode= 'r' , encoding= 'utf-8') as arquivo:
         leitor = csv.DictReader(arquivo)
         return list(leitor)
+
 
 def processar_dados(lista):
     categorias = {}
@@ -18,14 +19,36 @@ def processar_dados(lista):
     return categorias
 
 
-def calcular_vendas():
-    pass
+def calcular_vendas_por_categoria(dados_por_categoria):
+    resultado = {}
+    for categoria, itens in dados_por_categoria.items():
+        vendas_por_produto = {}
+        for item in itens:
+            produto = item.get('Categoria')
+            try:
+                valor = float(item.get('Venda', 0))
+            except ValueError:
+                valor = 0
+            if produto in vendas_por_produto:
+                vendas_por_produto[produto] += valor
+            else:
+                vendas_por_produto[produto] = valor
+        resultado[categoria] = vendas_por_produto
+    return resultado
 
-def exibir_resultados():
-    pass
+def exibir_resultados(lista):
+    #transformar em dataframe
+    #funcao melt unpivota um daframe
+    df = pd.DataFrame(lista)
+    df_reset = df.reset_index().rename(columns={'index': 'Origem'})
+    df_meltado = pd.melt(df_reset, id_vars=['Origem'], value_name='Valor')
+    df_meltado = df_meltado.dropna(subset=['Valor'])
+    df_meltado = df_meltado.drop(columns ='Origem')
+    print(df_meltado)
 
 
 if __name__ == "__main__":
     x = ler_Csv(path)
     y = processar_dados(x)
-    print(y)
+    z = calcular_vendas_por_categoria(y)
+    w = exibir_resultados(z)
